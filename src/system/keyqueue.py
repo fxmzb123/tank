e0c9cd70a3edb8ca97e04ee9527a6fc4c1d8a155
ming fu <fxmzb123@gmail.com>
2015-07-15 21:52:31 +1000

    Added doc string. Fixed errors in get_keys method.

--- src/system/keyqueue.py
+++ src/system/keyqueue.py
@@ -1,38 +1,46 @@
-from entities.enum import *
+'''This module contains class KeyQueue.
+'''
+
 
 class KeyQueue(object):
+    '''Keyqueue class provides method to only allow non-duplicated values.
+    This class is mainly used to prevent user press multiple keys at the
+    same time.
+    '''
     _keys = list()
     _size = 5
-    
+
     @classmethod
     def _is_key_allowed(cls, key):
+        '''Check whether the key is allow to enter the queue.
+        '''
         if key in cls._keys:
             return False
-            
+
         return True
-            
+
     @classmethod
     def pursh_key(cls, key):
+        '''Add the key to the queue. Only non-duplicated key can be addedd.
+        '''
         if len(cls._keys) <= cls._size:
             if cls._is_key_allowed(key):
                 cls._keys.append(key)
-    
+
     @classmethod
     def clean_keys(cls):
+        '''Remove all keys in the queue.
+        '''
         cls._keys[:] = []
-    
+
     @classmethod
     def remove_key(cls, key):
-        print cls._keys
+        '''Remove one specific key in the queue.
+        '''
         del cls._keys[cls._keys.index(key)]
-        
+
     @classmethod
     def get_keys(cls):
-        if len(cls._keys) == 0:
-            return ()
-        
-        if len(cls._keys) == 1:
-            return (cls._keys[0])
-        
-        if len(cls._keys) == 2:
-            return (cls._keys[0], cls._keys[1])
+        '''Get all keys.
+        '''
+        return tuple(cls._keys)
